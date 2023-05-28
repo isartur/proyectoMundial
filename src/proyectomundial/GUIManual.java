3a975d45231f64b20a075f44546b0beb4be9360f
islender <reyarturo.17@hotmail.com>
2023-05-28 14:54:46 -0500

    prueva

--- src/proyectomundial/GUIManual.java
+++ src/proyectomundial/GUIManual.java
@@ -21,7 +21,7 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
-import javax.swing.WindowConstants;
+import javax.swing.WindowConstants;//prueva
 
 public class GUIManual extends JFrame {
 
